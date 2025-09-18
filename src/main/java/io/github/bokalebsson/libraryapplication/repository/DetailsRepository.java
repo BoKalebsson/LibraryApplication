19642ec9951808565d5dd4ae8e323dae8878edbb
BoKalebsson <bo_kalebsson@hotmail.com>
2025-09-18 13:42:47 +0200

    Implemented specified methods for DetailsRepository.

--- src/main/java/io/github/bokalebsson/libraryapplication/repository/DetailsRepository.java
+++ src/main/java/io/github/bokalebsson/libraryapplication/repository/DetailsRepository.java
@@ -3,5 +3,15 @@ package io.github.bokalebsson.libraryapplication.repository;
 import io.github.bokalebsson.libraryapplication.entity.Details;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface DetailsRepository extends JpaRepository<Details, Integer> {
+
+    Optional<Details> findByEmail(String email);
+
+    List<Details> findByNameContaining(String mail);
+
+    List<Details> findByNameIgnoreCase(String name);
+
 }
