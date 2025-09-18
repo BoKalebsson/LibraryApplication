4f4a411f46955076a220ac1289fce4d6950ad82a
BoKalebsson <bo_kalebsson@hotmail.com>
2025-09-18 12:07:15 +0200

    Implemented specified methods for AppUserRepository.

--- src/main/java/io/github/bokalebsson/libraryapplication/repository/AppUserRepository.java
+++ src/main/java/io/github/bokalebsson/libraryapplication/repository/AppUserRepository.java
@@ -3,6 +3,18 @@ package io.github.bokalebsson.libraryapplication.repository;
 import io.github.bokalebsson.libraryapplication.entity.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
 public interface AppUserRepository extends JpaRepository<AppUser, Integer> {
+
+    Optional<AppUser> findByUsername(String username);
+
+    List<AppUser> findByRegDateBetween(LocalDate startDate, LocalDate endDate);
+
+    Optional<AppUser> findByUserDetails_Id(Integer detailsId);
+
+    Optional<AppUser> findByUserDetails_EmailIgnoreCase(String email);
 
 }
