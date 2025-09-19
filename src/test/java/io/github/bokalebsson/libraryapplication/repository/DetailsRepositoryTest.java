e5c364ef068f7e88495495869f267bd531d34d7f
BoKalebsson <bo_kalebsson@hotmail.com>
2025-09-19 14:34:32 +0200

    Implemented a @BeforeEach for testing purposes.

--- src/test/java/io/github/bokalebsson/libraryapplication/repository/DetailsRepositoryTest.java
+++ src/test/java/io/github/bokalebsson/libraryapplication/repository/DetailsRepositoryTest.java
@@ -1,4 +1,51 @@
 package io.github.bokalebsson.libraryapplication.repository;
 
+import io.github.bokalebsson.libraryapplication.entity.Details;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@DataJpaTest
+@ActiveProfiles("test")
 public class DetailsRepositoryTest {
+
+    @Autowired
+    private DetailsRepository detailsRepository;
+
+    private Details details1;
+    private Details details2;
+    private Details details3;
+
+    @BeforeEach
+    void setUp() {
+
+        detailsRepository.deleteAll();
+
+        details1 = Details.builder()
+                .name("Hans Luhrberg")
+                .email("hans.luhrberg@test.nu")
+                .birthDate(LocalDate.of(1979, 12, 13))
+                .build();
+
+        details2 = Details.builder()
+                .name("Greger Petterson")
+                .email("greger.pettersson@test.nu")
+                .birthDate(LocalDate.of(1976, 6, 17))
+                .build();
+
+        details3 = Details.builder()
+                .name("Hans Peterson")
+                .email("hans.peterson@test.nu")
+                .birthDate(LocalDate.of(1980, 3, 5))
+                .build();
+
+        detailsRepository.saveAll(List.of(details1, details2, details3));
+    }
+
+
+
 }
