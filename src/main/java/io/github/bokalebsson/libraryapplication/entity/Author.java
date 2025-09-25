a915c33ac4cbf4f920c97dfb7adb7f9ed22fb911
BoKalebsson <bo_kalebsson@hotmail.com>
2025-09-25 15:09:32 +0200

    Implemented Author-class.

---
+++ src/main/java/io/github/bokalebsson/libraryapplication/entity/Author.java
@@ -0,0 +1,35 @@
+package io.github.bokalebsson.libraryapplication.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "author")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @ManyToMany
+    @JoinTable(
+            name = "author_book",
+            joinColumns = @JoinColumn(name = "author_id"),
+            inverseJoinColumns = @JoinColumn(name = "book_id")
+    )
+    private Set<Book> books;
+
+}
